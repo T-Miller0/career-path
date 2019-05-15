8d97e56f4b8560673406c3c20e43a74f8d5c67a2
Your Name <thomas.j.c.miller@outlook.com>
2019-05-15 10:30:52 +0100

    burger ingredients added with prop types

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Aux from '../../hoc/Auxillery'
-import classes from './Layout.css'
+import Aux from '../../hoc/Auxillery';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
