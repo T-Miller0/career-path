b8eab11b327c6bb044ce54ffa9615b0820ebb434
Your Name <thomas.j.c.miller@outlook.com>
2019-07-10 11:05:35 +0100

    returns mostDesiredSkills

--- src/containers/Report/Report.js
+++ src/containers/Report/Report.js
@@ -7,14 +7,34 @@ import ShortTermGoal from '../../components/ShortTermGoal/ShortTermGoal';
 
 export class Store extends Component {
 
-  salaryDifference = (salary, desiredSalary) => (
-    (1 - (salary / desiredSalary)) * 100
-  )
+  salaryDifference = (salary, desiredSalary) => {
+    let percentage = (1 - (salary / desiredSalary)) * 100
+
+    return percentage.toFixed(0)
+  };
+
+  mostDesiredSkills = (usersSkills, roleSkills) => {
+    const allArrays = ([roleSkills.baselineSkills, roleSkills.specializedSkills, roleSkills.definingSkills, roleSkills.necessarySkills, roleSkills.distinguishingSkills]).flat()
+    const desiredSkills = []
+
+
+    for (var i = 0; i < allArrays.length; i++) {
+      for (var j = 0; j < usersSkills.length; j++)
+        if (allArrays[i].name === usersSkills[j]) {
+          allArrays.splice(i, 1)
+        }
+    }
+
+    const sortedArray = allArrays.sort((a, b) => parseFloat(a.count) - parseFloat(b.count))
+
+    return sortedArray
+  }
 
 
   render () {
-    console.log(reportData.careerPath[0])
-    console.log(reportData.currentRoleDetails.salaryMean, reportData.careerPath[0].details.meanSalary)
+    // console.log(reportData.careerPath[0].details.allSkills)
+    // console.log(reportData.careerPath[0].selectedSkills)
+    console.log(this.mostDesiredSkills(reportData.careerPath[0].selectedSkills, reportData.careerPath[0].details.allSkills))
 
     const diference = this.salaryDifference(reportData.currentRoleDetails.salaryMean, reportData.careerPath[0].details.meanSalary)
 
@@ -26,7 +46,7 @@ export class Store extends Component {
         <h1>Your short-term goal</h1>
         <ShortTermGoal
           details={reportData.careerPath[0]}
-          salaryPercentage={diference.toFixed(0) }/>
+          salaryPercentage={diference }/>
         <div>Long Term Goal</div>
       </div>
     )
